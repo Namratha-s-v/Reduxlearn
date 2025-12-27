fef2e5613cf840070e84f3af39db234dd0aa7cf8
Namratha S V <nammu836@gmail.com>
2025-12-27 20:16:32 +0530

    second commit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Userinfo from "./Userinfo";
 export default function App() {
   return (
     <div>
+      <h1>Welcome to the react redux ***</h1>
+      <h2>Hello Namratha!</h2>
       <Userinfo />
       <Button />
     </div>
